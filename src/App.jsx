46b670236d8eb6e830fc5ad82647ded07c31f37f
Ayush Sharma <sharmaayush4636@gmail.com>
2026-07-31 00:54:42 +0530

    Added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import Layout from './components/Layout'
 import ComingSoon from './pages/ComingSoon'
 import About from './pages/About'
