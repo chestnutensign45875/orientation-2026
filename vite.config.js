fadb4a728aaf2ed0d7e02dcdb46641b66129c196
Ayush Sharma <sharmaayush4636@gmail.com>
2026-08-10 13:17:31 +0530

    Optimizations

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2020',
+    cssCodeSplit: true,
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('framer-motion')) {
+            return 'vendor-framer'
+          }
+          if (id.includes('react-router') || id.includes('react-dom')) {
+            return 'vendor-react'
+          }
+        },
+      },
+    },
+  },
 })
